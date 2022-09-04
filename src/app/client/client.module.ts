984d0608dd6924fc0cd95481f54ea1fc8909fe8a
Benito Fernandez <laura.benito-fernandez@capgemini.com>
2022-09-04 17:34:34 +0200

    connect client with server to use dynamic data

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -17,6 +18,7 @@ import { ClientEditComponent } from './client-edit/client-edit.component';
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
     MatTableModule,
     MatIconModule, 
     MatButtonModule,
