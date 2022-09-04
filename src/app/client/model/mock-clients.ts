194c3cafa01b305323a52db4ae5c55221819b0d3
Benito Fernandez <laura.benito-fernandez@capgemini.com>
2022-09-04 11:04:12 +0200

    add clients views with mocked data

---
+++ src/app/client/model/mock-clients.ts
@@ -0,0 +1,9 @@
+import { Client } from "./Client";
+
+export const CLIENT_DATA: Client[] = [
+    { id: 1, name: "Margaret Brady" },
+    { id: 2, name: "Whoopi Willis" },
+    { id: 3, name: "Valentine Huff" },
+    { id: 4, name: "Tanner Miranda" },
+    { id: 5, name: "Cailin Mclaughlin" }
+]
